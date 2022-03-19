d7819277cc8008ef4061a76aff151be7e3d99544
Kern Handa <kern.handa@gmail.com>
2022-03-19 14:34:21 -0700

    Adds logging infra

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 A python port of https://github.com/kraiskil/onnx2c
-`
+
+Differences:
+* A quantized model should be created by a separate utility
--- onnx2c.py
+++ onnx2c.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python3
 
 import argparse
+import logging
 import onnx
 
 import graph
 
-def parse_args():
-    arg_parser = argparse.ArgumentParser(
-        description="Generate C code from an ONNX graph file")
+LOG_LEVELS = [logging.ERROR, logging.WARNING, logging.INFO, logging.DEBUG]
 
-    arg_parser.add_argument("input",
-                            help="Path to the ONNX file",
-                            default=None)
-    arg_parser.add_argument("output", help="Path to output file")
+
+def parse_args():
+    arg_parser = argparse.ArgumentParser(description="Generate C code from an ONNX graph file")
+
+    arg_parser.add_argument("input", help="Path to the ONNX file", default=None)
+    arg_parser.add_argument(
+        "-v", "--verbosity", type=int, choices=range(len(LOG_LEVELS)), help="Sets the logging verbosity", default=2
+    )
+    arg_parser.add_argument("-l", "--log", help="Specifies the file to write log to", default="onnx2c.log")
+    arg_parser.add_argument("output", help="Path to output file", default="-")
 
     args = vars(arg_parser.parse_args())
     return args
@@ -20,6 +25,7 @@ def parse_args():
 
 def main():
     args = parse_args()
+    logging.basicConfig(level=LOG_LEVELS[args["verbosity"]], filename=args["log"], filemode='w')
 
 
 if __name__ == "__main__":
